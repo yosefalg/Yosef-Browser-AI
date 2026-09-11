b8fb4c81a5f55d2b6cb8d3fa2ec8162b9f1ff7f0
cdd. <85797031+yosefalg@users.noreply.github.com>
2026-09-11 19:07:15 +0300

    fix: resolve launcher icon Expo config plugin import

--- plugins/withRaidLauncherIcon.js
+++ plugins/withRaidLauncherIcon.js
@@ -1,4 +1,4 @@
-const { withAndroidManifest, withDangerousMod } = require('@expo/config-plugins');
+const { withAndroidManifest, withDangerousMod } = require('expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
